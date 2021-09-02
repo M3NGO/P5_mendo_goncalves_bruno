8aab5d5bddd4c86a2c47c3c01bed30449bf7f196
M3NGO <82385710+M3NGO@users.noreply.github.com>
2021-09-02 18:01:58 +0200

    script_cart.js
    Ajout check si  tableauStorage est vide ou non et alert user si tel est le cas
    Ajout class bootstrap pour mise en page

--- script_cart.js
+++ script_cart.js
@@ -4,6 +4,18 @@ let title = document.title; // ajout titre page dynamique selon produit
 let tableauStorage = JSON.parse(localStorage.getItem("tableauStorage")); //on parse le localStorage dans un tableau appellé tableauStorage
 console.table(tableauStorage)
 
+//check si localStorage vide et affiche alerte panier vide pour retour accueil
+if (localStorage.getItem("tableauStorage") === null) {
+    alert( 'Votre panier est vide')
+    window.location = 'http://127.0.0.1:5500/index.html' // retour à l'accueil au click panier alert
+  }
+
+
+//if pour checker si tableauStorage est vide si oui alors on clear la memoire et reload la page pour afficher le message panier vide
+  if (tableauStorage.length <= 0){
+    window.localStorage.clear()
+    document.location.reload()
+}
 //ArrayUnique pour display uniquement une fois les appareils souahités (les quantités sont dans colonne inCart du tableauStorage)
 let arrayUnique = Array.from(new Set(tableauStorage.map(appareil => appareil._id))) // creation nouveau tableau a partir d'un nouveau set d'ids uniques provenant du tableauStorage
                     .map(_id => { 
@@ -21,39 +33,69 @@ let arrayUnique = Array.from(new Set(tableauStorage.map(appareil => appareil._id
         let objetNom = element.nom
         
         let cardDiv = document.createElement("div") // créé une div reprensentant la card des produits
-            cardDiv.setAttribute('class', 'card')
+            cardDiv.setAttribute('class', 'card card-extend')
             document.querySelector('.panier').appendChild(cardDiv);
             
         let img = new Image(); // display des images
             img.src = element.img; //donne les urls des photos dans du JSON
             img.alt = element.nom + ' ' + element.prix + ' €';
             img.setAttribute('id', 'image');
+            img.setAttribute('class', 'card-img-top')
             document.querySelector('.panier').appendChild(cardDiv).appendChild(img);// display des photos en tant qu'img dans le html
-            console.log(img)
+        
+        let cardSousImage = document.createElement("div")
+            cardSousImage.setAttribute('class','card-body')
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage)
+
+        let cardTitreGrid = document.createElement("div")
+            cardTitreGrid.setAttribute('class','row')
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage).appendChild(cardTitreGrid)
+
+        let cardTitre = document.createElement("div")
+            cardTitre.setAttribute('class', ' mb-2 col-sm')
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage).appendChild(cardTitreGrid).appendChild(cardTitre)
+        
         let nomObjet = document.createElement("h2") // créé un élément HTML H2 pour le nom du produit
             nomObjet.textContent = element.nom;
-            document.querySelector('.panier').appendChild(cardDiv).appendChild(nomObjet);
+            nomObjet.setAttribute('class', 'card-title')
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage).appendChild(cardTitreGrid).appendChild(cardTitre).appendChild(nomObjet);
             console.log(nomObjet)
                     
-        let prixObjet = document.createElement("h3") // créé un élément HTML H3 pour le prix du produit
+        let prixObjet = document.createElement("p") // créé un élément HTML H3 pour le prix du produit
             prixObjet.textContent = (prixObjetUnique + ' €'); // .toFixed(2) pour mettre la virgule et deux chiffres après
-            document.querySelector('.panier').appendChild(cardDiv).appendChild(prixObjet);
+            prixObjet.setAttribute('class', 'card-text')
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage).appendChild(cardTitreGrid).appendChild(cardTitre).appendChild(prixObjet);
             console.log(prixObjet)
 
+        let cardDivSelection = document.createElement("div")
+            cardDivSelection.setAttribute('class','d-flex row')
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage)
+
         let quantite = document.createElement("input");
-            quantite.setAttribute('class', 'quantite')
+            quantite.setAttribute('class', 'quantite mb-1')
             quantite.setAttribute('type', 'number')
             quantite.setAttribute('value', quantiteArrayUnique)
             quantite.setAttribute('min', '1') // Qté minimum 1 pour empecher des values inférieures, si le user veut delete l'objet alors il y a un bouton pour ça
             quantite.setAttribute('max', '10') // qté max de 10 on suppose que le stock est de 10
             quantite.placeholder = 'Choisissez la quantité'
-            document.querySelector('.panier').appendChild(cardDiv).appendChild(quantite); // ajout de menuOptions dans les menuDéroulants
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage).appendChild(cardDivSelection).appendChild(quantite); // ajout de menuOptions dans les menuDéroulants
+
+        let tableauTotalParAppareil = document.createElement("div");
+            tableauTotalParAppareil.setAttribute ('class', 'tableauTotal col-sm')
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage).appendChild(cardTitreGrid).appendChild(tableauTotalParAppareil)
+
+        let prixTotalApprareil = document.createElement("p"); 
+            prixTotalApprareil.setAttribute('class', 'prixTotalAppareil card-title')
+            prixTotalApprareil.innerHTML = 'Total Appareil : '+((quantite.value)*(element.prix)).toFixed(2)+' ' + '€';
+            console.log(prixTotalApprareil.innerHTML)
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage).appendChild(cardTitreGrid).appendChild(tableauTotalParAppareil).appendChild(prixTotalApprareil); // ajout de menuOptions dans les menuDéroulants
+    
 
         let boutonValiderQuantiteAppareils = document.createElement("button");
             boutonValiderQuantiteAppareils.textContent = 'Valider la quantité';
             boutonValiderQuantiteAppareils.setAttribute('class', 'boutonValider')
             boutonValiderQuantiteAppareils.setAttribute('class', 'btn btn-primary')
-            document.querySelector('.panier').appendChild(cardDiv).appendChild(boutonValiderQuantiteAppareils);
+            document.querySelector('.panier').appendChild(cardDiv).appendChild(cardSousImage).appendChild(cardDivSelection).appendChild(boutonValiderQuantiteAppareils);
         
        boutonValiderQuantiteAppareils.addEventListener('click', function (){
 
@@ -70,10 +112,11 @@ let arrayUnique = Array.from(new Set(tableauStorage.map(appareil => appareil._id
                                                             console.log(arrayUnique)
                             }); //fermeture addEventListener click boutonValiderQuantiteAppareils
                             
-        let boutonX = document.createElement("button");//bouton enlever appareil du panier
-            boutonX.setAttribute('class','btn btn-close')
+        let boutonX = document.createElement("a");//bouton enlever appareil du panier
+            boutonX.setAttribute('class','annuler')
             boutonX.setAttribute('aria-label', 'Annuler')
-            boutonX.setAttribute('title', 'Annuler Objet')
+            boutonX.setAttribute('title', 'Supprimer du panier')
+            boutonX.innerHTML= '<i class="fas fa-times"></i>'
             document.querySelector('.panier').appendChild(cardDiv).appendChild(boutonX);
             
             boutonX.addEventListener('click', function(){
@@ -82,22 +125,12 @@ let arrayUnique = Array.from(new Set(tableauStorage.map(appareil => appareil._id
                                 console.log(appareilFind)
                                 document.location.reload()
 
+
                                 arrayUniqueFiltre = arrayUnique.splice(arrayUnique.indexOf(appareilFind) , 1)//creation nouveau tableau arrayuniquefiltre pour stocker la valeur qu'on retire du tableau arrayUnique
 
                                 localStorage.setItem("tableauStorage", JSON.stringify(arrayUnique)) // on set le nouveau tableau arrayunique sans larrayuniquefiltre dans le tableauStorage du localstorage pour remplacer l'ancien tableau par le nouveau
                                 tableauStorage = JSON.parse(localStorage.getItem("tableauStorage")); // on parse le nouveau tableauStorage pour recréer la page du panier
                             })// fermeture addEventListener boutonX
-                                
-
-        let tableauTotalParAppareil = document.createElement("div");
-            tableauTotalParAppareil.setAttribute ('class', 'tableauTotal')
-            document.querySelector('.panier').appendChild(cardDiv).appendChild(tableauTotalParAppareil)
-
-        let prixTotalApprareil = document.createElement("p"); 
-            prixTotalApprareil.setAttribute('class', 'prixTotalAppareil')
-            prixTotalApprareil.innerHTML = ((quantite.value)*(element.prix)).toFixed(2)+' ' + '€';
-            console.log(prixTotalApprareil.innerHTML)
-            document.querySelector('.panier').appendChild(cardDiv).appendChild(tableauTotalParAppareil).appendChild(prixTotalApprareil); // ajout de menuOptions dans les menuDéroulants
     
     })//fermeture for each arrayUnique.id
 
@@ -110,7 +143,7 @@ localStorage.setItem("coutTotalCommande", CoutTotalCommande.toFixed(2))
 //fonction totalCommande pour obtenir le total de la commande tous objets et quantités désirées
 function totalCommande(){
     let totalCommande = document.createElement("div")
-    totalCommande.setAttribute('class', 'totalCommande')
+    totalCommande.setAttribute('class', 'totalCommande fs-2')
     totalCommande.innerHTML ='Total de votre commande : '+ CoutTotalCommande.toFixed(2) + ' ' + '€'
     document.querySelector('.totalPanier').appendChild(totalCommande)
 }//fermeturef fonction totalCommande
@@ -120,7 +153,7 @@ let validerCommande = document.createElement("button")
 validerCommande.textContent = 'Finaliser la commande';
 validerCommande.setAttribute('class', 'bouton_validerCommande')
 validerCommande.setAttribute('title', 'Cliquez pour valider la commande')
-validerCommande.setAttribute('class', 'btn btn-primary')
+validerCommande.setAttribute('class', 'btn btn-primary mt-3')
 document.querySelector('.formulaire').appendChild(validerCommande)
 
 //check validity formulaire :
@@ -186,3 +219,5 @@ validerCommande.addEventListener("click", function (){
         } // \n pour sauter ligne dans l'alerte
         
 })//fermeture checkValidity.addEventListener click
+
+
