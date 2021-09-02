b67c673f03ebcc15d38d6d79b6bc99e77fc4dfa6
M3NGO <82385710+M3NGO@users.noreply.github.com>
2021-09-02 17:59:22 +0200

    confirmation_commande
    ajout header et mise forme de l'apparence de la page

--- script_confirmation_commande.js
+++ script_confirmation_commande.js
@@ -2,74 +2,39 @@
 let title = document.title; // ajout titre page dynamique selon produit
 document.title = '0rinoco - Vous remercie pour votre commande'; // ajout titre page dynamique selon produit
 
-
 let contactparse = JSON.parse(localStorage.getItem("contact"))
 console.log(contactparse)
 
 let commandeFinale = JSON.parse(localStorage.getItem("tableauStorage"))
 console.log(commandeFinale)
 
-let orderId = document.createElement("h1")
-orderId.setAttribute('class', 'ID')
-document.querySelector('.confirmationCommande').appendChild(orderId);
-orderId.innerHTML = 'Votre numéro de commande est le : '+localStorage.orderId
-
-let merci = document.createElement('h2')
-merci.setAttribute('class', 'merci')
-merci.innerHTML = 'Merci pour votre commande' 
-document.querySelector('.confirmationCommande').appendChild(merci);
-
-
-let prenom = document.createElement("h3")
-prenom.setAttribute('class', 'prenom')
-document.querySelector('.confirmationCommande').appendChild(prenom);
-prenom.innerHTML = contactparse.firstName
-
-let nom = document.createElement('h3')
-nom.setAttribute('class', 'nom')
-document.querySelector('.confirmationCommande').appendChild(nom)
-nom.innerHTML = contactparse.lastName
-
-// let addresse = document.createElement('p')
-// addresse.setAttribute('class', 'addresse')
-// document.querySelector('.confirmationCommande').appendChild(addresse)
-// addresse.innerHTML = contactparse.address
-
-// let codePostal = document.createElement('p')
-// codePostal.setAttribute('class','codePostal')
-// document.querySelector('.confirmationCommande').appendChild(codePostal)
-// codePostal.innerHTML = contactparse.postal
-
-// let ville = document.createElement('p')
-// ville.setAttribute('class','ville')
-// document.querySelector('.confirmationCommande').appendChild(ville)
-// ville.innerHTML = contactparse.city
-
-// let email = document.createElement('p')
-// email.setAttribute('class', 'email')
-// document.querySelector('.confirmationCommande').appendChild(email)
-// email.innerHTML = contactparse.email
-
-
-
+let remerciement = document.createElement('h1')
+    remerciement.setAttribute('class', 'text-center mb-4')
+    remerciement.innerHTML= 'vous remercie pour votre commande N° :'+'<br/>'+localStorage.orderId 
+    document.querySelector('.container-fluid').appendChild(remerciement)
 
 //commande:
-let quantite =document.createElement('p')
-quantite.setAttribute('class', 'quantite')
-document.querySelector('.confirmationCommande').appendChild(quantite)
 let quantiteFinale = localStorage.getItem('coutTotalCommande')
 console.log(quantiteFinale)
 
-quantite.innerHTML = 'Le montant total de votre commande est de : ' + quantiteFinale + ' ' + '€'
+let confPaiement = document.createElement('p')
+    confPaiement.setAttribute('class', 'quantite')
+    confPaiement.innerHTML = contactparse.firstName + ' '+ contactparse.lastName + ' ' + 'votre paiement de ' + quantiteFinale + ' ' + '€' + ' a été accepté !' 
+    document.querySelector('.container-fluid').appendChild(confPaiement)
+
+let confAdresse = document.createElement('p')
+    confAdresse.setAttribute('class', '')
+    confAdresse.innerHTML = "Vous recevrez votre commande sous peu à l'adresse indiquée : " + '<br/>' + contactparse.address + ' ' + contactparse.postal + ' ' + contactparse.city
+    document.querySelector('.container-fluid').appendChild(confAdresse)
 
 let boutonFinal = document.createElement('button')
-boutonFinal.setAttribute('class','boutonFinal')
-boutonFinal.setAttribute('onclick', 'window.location.replace("http://127.0.0.1:5500/index.html")')
+    boutonFinal.setAttribute('class','btn btn-primary')
+    boutonFinal.setAttribute('onclick', 'window.location.replace("http://127.0.0.1:5500/index.html")')
 // fonction pour clear localStorage au click du retour a l'accueil
 boutonFinal.addEventListener('click', function(){
     localStorage.clear();
 })
-document.querySelector('.confirmationCommande').appendChild(boutonFinal)
+document.querySelector('.container-fluid').appendChild(boutonFinal)
 boutonFinal.innerHTML = "retour à l'accueil"
 
 //Nettoyer localStorage a la fermeture de la page
