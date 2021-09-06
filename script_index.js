59d7760e065ac166c6bc99ec1a4d363f5d5ef287
M3NGO <82385710+M3NGO@users.noreply.github.com>
2021-09-06 17:46:35 +0200

    script_index.js
    comment console.log

--- script_index.js
+++ script_index.js
@@ -1,6 +1,6 @@
 
 const url =	"http://localhost:3000/api/cameras/";
-console.log(url)
+// console.log(url)
 
 let title = document.title; // ajout titre page dynamique selon produit
 document.title = '0rinoco'; // ajout titre page dynamique selon produit
@@ -14,30 +14,28 @@ let texteImages =  async function() { //fonction asynchrone pour laisser charger
     let response = await fetch(url)
         .then((response)=>{
             let itemData = response.json(); // renvoit la promesse en JSON
-                    console.log(response.body)
+                    // console.log(response.body)
                 appareils.push(itemData); //envoit les réponses reçues de JSON dans tableau appareils recenssant tous les appareils
-                    console.log(appareils)
+                    // console.log(appareils)
 
                 itemData.then(function (appareil){
                     appareils.push(...appareil); // (opérateur de décomposition  https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Spread_syntax) Array appareil contenant chaque élément du Json "...appareil" pour importer tous les appareils dans le tableau appareil
-                        console.table(appareil)
+                        // console.table(appareil)
 
                     appareil.forEach((appareilPhoto) => {
-                        console.log(appareilPhoto)
+                        // console.log(appareilPhoto)
 
                         let cardDiv = document.createElement("div"); // créé une div reprensentant la card des produits
                             cardDiv.setAttribute('class', 'card');
                             cardDiv.setAttribute('class', 'card align-self-center');
                             document.querySelector('.section').appendChild(cardDiv);
-
-                            console.log(appareilPhoto.name + " " + appareilPhoto.price + appareilPhoto.imageUrl) // pour chaque élément reçu du JSON et enregistré dans notre array appareil, on va créer des images, texte, titre, prix issu du JSON et jouer sur le DOM pour les afficher dans le navigateur
+                            // console.log(appareilPhoto.name + " " + appareilPhoto.price + appareilPhoto.imageUrl) // pour chaque élément reçu du JSON et enregistré dans notre array appareil, on va créer des images, texte, titre, prix issu du JSON et jouer sur le DOM pour les afficher dans le navigateur
                        
                         let img = new Image(); // display des images
                             img.src = appareilPhoto.imageUrl; //donne les urls des photos dans du JSON
                             img.alt = appareilPhoto.name + ' ' + (appareilPhoto.price/100).toFixed(2) + ' €'; //adaptation de la description selon recommendation WAVE car ALT DESCRIPTION trop longue
-                            img.setAttribute('class','clickable card-img-top');
+                            img.setAttribute('class','card-img-top');
                             img.setAttribute('onclick', 'location.href='+ "'" + 'produit.html?id=' + appareilPhoto._id + "'" + ';'); // creation du lien cliquable personnalisé
-                            
                             document.querySelector('.section').appendChild(cardDiv).appendChild(img);// display des photos en tant qu'img dans le html
                         
                         let texteCarteObjet = document.createElement('div');
@@ -47,13 +45,13 @@ let texteImages =  async function() { //fonction asynchrone pour laisser charger
                             nomObjet.textContent = appareilPhoto.name;
                             nomObjet.setAttribute('class','card-title')
                             document.querySelector('.section').appendChild(cardDiv).appendChild(texteCarteObjet).appendChild(nomObjet);
-                                console.log(nomObjet)
+                                // console.log(nomObjet)
                         
                         let prixObjet = document.createElement("p"); // créé un élément HTML H3 pour le prix du produit
                             prixObjet.textContent = (appareilPhoto.price/100).toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') + ' €'; // .toFixed(2) pour mettre la virgule et deux chiffres après + .replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') pour mettre separateur miliers et virgule au lieux du point
                             prixObjet.setAttribute('class','card-text')
                             document.querySelector('.section').appendChild(cardDiv).appendChild(texteCarteObjet).appendChild(prixObjet);
-                                console.log(prixObjet)
+                                // console.log(prixObjet)
 
                     });// fermeture appareils.forEach appareilPhoto dans tableau appareil
 
